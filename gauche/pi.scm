4d69e0fa48af5be8ca6bb093d8dd0caf041aeb50
Philip Chu <technicat@users.noreply.github.com>
2022-05-07 16:58:35 -0700

    calculate pi

---
+++ gauche/pi.scm
@@ -0,0 +1,18 @@
+(use srfi-27)
+
+(define (pi radius) ((piter radius 0 0)))
+
+(define (piter radius inside total)
+  (print (* 4.0 (/ inside (+ 1.0 total))))
+  (if (incircle? (random radius) (random radius) radius)
+      (piter radius (+ 1 inside) (+ 1 total))
+      (piter radius inside (+ 1 total))))
+
+(define (random range)
+  (- (* (random-real) (+ range range)) range))
+
+(define (incircle? x y radius)
+  (< (+ (square x) (square y)) (square radius)))
+
+(define (square x) (* x x))
+
