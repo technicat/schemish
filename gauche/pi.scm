64c213f965afb1df153f79443538c88b5ffa4db6
Philip Chu <technicat@users.noreply.github.com>
2022-05-07 17:15:22 -0700

    Update pi.scm

--- gauche/pi.scm
+++ gauche/pi.scm
@@ -3,7 +3,7 @@
 (define (pi radius) ((piter radius 0 0)))
 
 (define (piter radius inside total)
-    (print (* 4.0 (/ inside (+ 0.01 total))))
+    (print (* 4.0 (/ inside (+ 0.000000001 total))))
     (if (incircle? (random radius) (random radius) radius)
         (piter radius (+ 1 inside) (+ 1 total))
         (piter radius inside (+ 1 total))))
