d400e3c8570244a915033f6fa692583991b892f4
Philip Chu <technicat@users.noreply.github.com>
2022-07-26 22:46:06 -0700

    Update pi.scm

--- pi/pi.scm
+++ pi/pi.scm
@@ -1,19 +1,25 @@
 (use srfi-27)
 
-(define (pi radius) ((piter radius 0 0)))
+(define pi 
+  (lambda (radius) 
+    (piter radius 0 0)))
 
-(define (piter radius inside total)
+(define piter
+  (lambda (radius inside total)
     (print (* 4.0 (/ inside (+ 0.000000001 total))))
     (if (incircle? (random radius) (random radius) radius)
         (piter radius (+ 1 inside) (+ 1 total))
-        (piter radius inside (+ 1 total))))
+        (piter radius inside (+ 1 total)))))
 
-(define (random range)
-  (- (* (random-real) (+ range range)) range))
+(define random
+  (lambda (range)
+    (- (* (random-real) (+ range range)) range)))
 
-(define (incircle? x y radius)
-  (< (+ (square x) (square y)) (square radius)))
+(define incircle? 
+  (lambda (x y radius)
+    (< (+ (square x) (square y)) (square radius))))
 
-(define (square x) (* x x))
+(define square
+  (lambda (x) (* x x)))
 
 
