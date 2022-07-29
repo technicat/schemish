ab0f5b0b1d30d1e257071224b75d219ccb9eac08
Philip Chu <technicat@users.noreply.github.com>
2022-07-28 19:10:53 -0700

    Update scheme.scm

--- format/scheme.scm
+++ format/scheme.scm
@@ -11,23 +11,37 @@
                 (if (eof-object? x)
                     '()
                      (begin
-                        (format-list x)
+                        (format-exp x 0)
                         (newline)
                         (format-input p))))))
 
+(define format-exp
+    (lambda (exp indent)
+        (if (list? exp)
+                (begin  
+                (newline)
+                (spaces indent)
+                (write-char #\()
+                (format-list exp (+ 1 indent)))
+            (write exp))))
+
 (define format-list
-    (lambda (exp)
+    (lambda (exp indent)
         (cond ((null? exp)
                 (begin 
                    (write-char #\))
                     ))
-            ((list? exp)
-            (begin
-                (write-char #\()
-                (write (car exp))
-                (format-list (cdr exp))))
             (else
+            (format-exp (car exp) indent)
+            (spaces 1)
+            (format-list (cdr exp) indent)))))
+
+(define spaces
+    (lambda (num)
+        (let f ((x num))
+            (if (> x 0)
                 (begin
-                (print " ")
-                (write exp ))))))
+                    (write-char #\ )
+                     (f (- x 1)))))))
+
            
