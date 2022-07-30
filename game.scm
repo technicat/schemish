4390fc4196a77b2ae39aabfbffdad39e6ae21baa
Philip Chu <technicat@users.noreply.github.com>
2022-07-30 11:10:19 -0700

    Create game.scm

---
+++ game.scm
@@ -0,0 +1,13 @@
+(define-class <game> () ())
+
+(define-class <boardgame> () ())
+
+(define-class <tictactoe> (<boardgame>) ())
+
+(define-class <othello> (<boardgame>) ())
+
+(define-class <checkers> (<boardgame>) ())
+
+(define-class <chess> (<boardgame>) ())
+
+(define-class <chinesechess> (<boardgame>) ())
