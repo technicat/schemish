7c1144a729ea0640f46ca10fc5a0406ad26c50aa
Philip Chu <technicat@users.noreply.github.com>
2022-07-31 18:18:13 -0700

    Update count.scm

--- count.scm
+++ count.scm
@@ -1,6 +1,7 @@
 #!/usr/local/bin/gosh
 
 (use gauche.parseopt) ; command line args
+(use file.util)
 
 (define (main args)
   (let-args (cdr args)
@@ -8,12 +9,30 @@
        . restargs
       )
     (if f
-        (count-file f))))
+        (count-file f)
+        (count-current-directory))))
+
+(define count-current-directory
+    (lambda ()
+        (count-directory (current-directory))))
+
+(define count-directory
+    (lambda (dir)
+        (count-files (directory-list dir))))
+
+(define count-files
+    (lambda (files)
+        (if (not (null? files))
+            (begin 
+                (count-file (car files))
+                (count-files (cdr files))))))
 
 (define count-file
     (lambda (file)
         (print file)
-        (call-with-input-file file count-input)))
+        (if (file-is-directory? file)
+            (count-directory file)
+            (call-with-input-file file count-input))))
 
 (define count-input
     (lambda (p)
