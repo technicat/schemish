e89e863acac8a4a47eddd607abcf7a5345a12562
Philip Chu <technicat@users.noreply.github.com>
2022-08-06 17:34:06 -0700

    Update pi.scm

--- pi.scm
+++ pi.scm
@@ -4,32 +4,32 @@
 (use srfi-27) ; random-real
 
 (define (main args)
-  (let-args (cdr args)
-      ((h "h|help")
-        (r "r|radius=i" 100)
-       . restargs
-      )
-      (if h
-         (print "pi.scm -r radius")
-          (pi r))))
+ (let-args (cdr args)
+  ((h "h|help")
+   (r "r|radius=i" 100)
+   . restargs
+   )
+  (if h
+   (print "pi.scm -r radius")
+   (pi r))))
 
-(define pi 
-  (lambda (radius) 
-    (let f ((inside 0)
-            (total 0))
-      (print (* 4.0 (/ inside (+ 0.000000001 total))))
-      (if (incircle? (random radius) (random radius) radius)
-        (f (+ 1 inside) (+ 1 total))
-        (f inside (+ 1 total))))))
+(define pi
+ (lambda (radius)
+  (let f ((inside 0)
+          (total 0))
+   (print (* 4.0 (/ inside (+ 0.000000001 total))))
+   (if (incircle? (random radius) (random radius) radius)
+    (f (+ 1 inside) (+ 1 total))
+    (f inside (+ 1 total))))))
 
 (define random
-  (lambda (range)
-    (- (* (random-real) (+ range range)) range)))
+ (lambda (range)
+  (- (* (random-real) (+ range range)) range)))
 
-(define incircle? 
-  (lambda (x y radius)
-    (< (+ (square x) (square y)) (square radius))))
+(define incircle?
+ (lambda (x y radius)
+  (< (+ (square x) (square y)) (square radius))))
 
 (define square
-  (lambda (x) (* x x)))
+ (lambda (x) (* x x)))
 
