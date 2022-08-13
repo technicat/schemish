b8eae80654999cc81bac3353364cc3411b5b2f40
Philip Chu <technicat@users.noreply.github.com>
2022-08-13 11:38:10 -0700

    Update pi.scm

--- pi.scm
+++ pi.scm
@@ -13,23 +13,17 @@
    (print "pi.scm -r radius")
    (pi r))))
 
-(define pi
- (lambda (radius)
-  (let f ((inside 0)
-          (total 0))
-   (print (* 4.0 (/ inside (+ 0.000000001 total))))
-   (if (incircle? (random radius) (random radius) radius)
-    (f (+ 1 inside) (+ 1 total))
-    (f inside (+ 1 total))))))
+(define (pi radius)
+ (let f ((inside 0)
+         (total 0))
+  (print (* 4.0 (/ inside (+ 0.000000001 total))))
+  (if (incircle? (random radius) (random radius) radius)
+   (f (+ 1 inside) (+ 1 total))
+   (f inside (+ 1 total)))))
 
-(define random
- (lambda (range)
-  (- (* (random-real) (+ range range)) range)))
+(define (random range)
+ (- (* (random-real) (+ range range)) range))
 
-(define incircle?
- (lambda (x y radius)
-  (< (+ (square x) (square y)) (square radius))))
-
-(define square
- (lambda (x) (* x x)))
+(define (incircle? x y radius)
+ (< (+ (square x) (square y)) (square radius)))
 
